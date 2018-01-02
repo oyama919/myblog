79d0a4b18850dc201959b1d36799152be5edc3fb
oyama919 <oyama919@gmail.com>
2018-01-02 17:14:47 +0900

    form test

--- routes/show.js
+++ routes/show.js
@@ -3,9 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var name = req.query.name;
+  var email = req.query.email;
   var data = {
     title: 'show',
-    content: 'this is show page'
+    content: 'this is show page',
+    message:''
+  }
+  res.render('show', data);
+});
+router.post('/post', function(req, res, next) {
+  var msg = req.body['message'];
+  var data = {
+    title: 'show',
+    content: 'this is show page posted!',
+    message: msg
   }
   res.render('show', data);
 });
