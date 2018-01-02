d758700a7268bbce2bf72df8ea2ff69a5e738690
oyama919 <oyama919@gmail.com>
2018-01-02 19:01:50 +0900

    RSS test

--- routes/show.js
+++ routes/show.js
@@ -1,27 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+var http = require('https');
+var parseString = require('xml2js').parseString;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var name = req.query.name;
-  var email = req.query.email;
-  msg=(req.session.message != undefined)? 'latest message:  '+req.session.message:'';
-  var data = {
-    title: 'show',
-    content: 'this is show page',
-    message:msg
-  }
-  res.render('show', data);
-});
-router.post('/post', function(req, res, next) {
-  var msg = req.body['message'];
-  req.session.message = msg;
-  var data = {
-    title: 'show',
-    content: 'this is show page posted!',
-    message: req.session.message
-  }
-  res.render('show', data);
+  var opt = {
+    host: 'news.google.com',
+    port: 443,
+    path: '/news?hl=ja&ned=us&id=UTF-8&oe=UTF-8&output=rss'
+  };
+  http.get(opt, (res2) => {
+    var body ='';
+    res2.on('data', (data) => {
+      body += data;
+    });
+    res2.on('end', () => {
+      parseString(body.trim(),(err, result) => {
+        var data = {
+          title: 'title',
+          content: result.rss.channel[0].item
+        };
+        res.render('show',data);
+      });
+    });
+  });
 });
 
 module.exports = router;
