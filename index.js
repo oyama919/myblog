01b9f28f3b03c025dc2077d34f7090870212f763
oyama919 <oyama919@gmail.com>
2018-01-02 16:11:17 +0900

    add Body Parser and test

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 var express = require('express');
 var app = express();
 var ejs = require("ejs");
+var bodyParser = require('body-parser');
 
 app.engine('ejs', ejs.renderFile);
 app.use(express.static('assets'));
+app.use(bodyParser.urlencoded({extended: false}));
 
+var data = {
+  'Data1':"test data 1",
+  'Data2':"test data 2",
+  'Data3':"test data 3"
+};
 app.get('/', (req,res) => {
+  var msg = 'No Message';
   res.render('index.ejs',
     {
       title:'Index Page',
       content:'Index Page Contents',
+      message: msg,
+      data:data,
+      link:{href:'/show',text:'show page'}
+    }
+  );
+});
+
+app.post('/',(req,res) => {
+  var msg = req.body.massage;
+  res.render('index.ejs',
+    {
+      title:'Index Page',
+      content:'Index Page Contents',
+      message: msg,
+      data:data,
       link:{href:'/show',text:'show page'}
     }
   );
@@ -20,6 +43,8 @@ app.get('/show', (req,res) => {
     {
       title:'Show Page',
       content:'Show Page Contents',
+      message: '',
+      data:'',
       link:{href:'/',text:'index page'}
     }
   );
