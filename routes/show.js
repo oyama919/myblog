78e1e17f08bf832b9206608a18bff26cbabd639f
oyama919 <oyama919@gmail.com>
2018-01-02 20:01:24 +0900

    sqlite3追加　仮データ呼出

--- routes/show.js
+++ routes/show.js
@@ -2,30 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 var http = require('https');
-var parseString = require('xml2js').parseString;
+var sqlite3 = require('sqlite3');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var opt = {
-    host: 'news.google.com',
-    port: 443,
-    path: '/news?hl=ja&ned=us&id=UTF-8&oe=UTF-8&output=rss'
-  };
-  http.get(opt, (res2) => {
-    var body ='';
-    res2.on('data', (data) => {
-      body += data;
-    });
-    res2.on('end', () => {
-      parseString(body.trim(),(err, result) => {
+
+  var db = new sqlite3.Database('myblog.sqlite3');
+
+  db.serialize(() => {
+    db.all("select * from mydata", (err, rows) => {
+      if (!err){
         var data = {
           title: 'title',
-          content: result.rss.channel[0].item
-        };
-        res.render('show',data);
-      });
+          content: rows
+        }
+        res.render('show', data);
+      }
     });
   });
+
 });
 
 module.exports = router;
