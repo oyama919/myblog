b5f15b674f5b86e9289f15f42ce821121dd97a30
oyama919 <oyama919@gmail.com>
2018-01-03 11:41:51 +0900

    express server PORT=3001 node bin/www and react_client npm start

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json([
+    {id:1, username: "hoge"},
+    {id:2, username: "fuga"},
+  ]);
 });
 
 module.exports = router;
